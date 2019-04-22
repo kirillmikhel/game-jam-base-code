f4b248617be9aee174c5aaaf698e894f4c2cc910
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-04-22 22:33:31 +0200

    Add the rest 😅

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int score = 0;
+    public bool isGameOn = false;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartTheGame()
     {
+        Instance.isGameOn = true;
         
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
+    public static void Restart()
+    {
+        Instance.isGameOn = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public static void TriggerVictory()
+    {
+        Instance.isGameOn = false;
+        SceneManager.LoadScene("Victory");
+    }
+    
+    public static void TriggerGameOver()
+    {
+        Instance.isGameOn = false;
+        SceneManager.LoadScene("GameOver");
     }
 }
